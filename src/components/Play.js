4eef67c9228c6d1f13e0f115e73bc3bed44dab67
Adam Leskis <leskis@gmail.com>
2020-06-27 16:41:47 +0100

    spell react

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,4 +1,4 @@
-import Reacte from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
